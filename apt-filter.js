106c2351fcb126879a7cf184a25032577587bb6e
Jona <jonathanfarache@gmail.com>
2018-08-28 19:46:14 +0300

    starter code

---
+++ apt-filter.js
@@ -0,0 +1,16 @@
+//Only start playing with this file if you're done with all the basics
+
+const must = (radio) => radio.filter(rb => rb.checked).length
+
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+    let relevantApts = apartments.filter(a =>
+        a.address.toLowerCase().includes(address.toLowerCase() || "") &&
+        a.price >= (minPrice || 0) &&
+        a.price <= (maxPrice || a.price) &&
+        a.rooms >= (minRooms || 0) &&
+        a.rooms <= (maxRooms || a.rooms)
+    )
+
+    immediate = !immediate[0].checked
+    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+
+$("button").on("click", function () {
+    let address = $("#addr-input").val()
+    let minPrice = $("#min-p-input").val()
+    let maxPrice = $("#max-p-input").val()
+    let minRooms = $("#min-r-input").val()
+    let maxRooms = $("#max-r-input").val()
+    let immediate = $("#immed-y")
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    renderApts(relevantApts)
+})
+
+const renderApts = function (apartments) {
+    $("#results").empty()
+    console.log(apartments) //array of apartments to render
+    //Your code goes here.
+}
+
+renderApts(apartments) //renders apartments when page loads
